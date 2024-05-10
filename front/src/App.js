d8aec4d2bcc9d6538de3a4e0412e4874cb824d4b
rebecca98m <schiavonrebecca@live.com>
2024-05-10 18:29:51 +0200

    fix italiano

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./img/logo/primary-logo.png";
+
+function App() {
+  return (
+    <div className="App">
+        <div>
+            <img src={logo} width={200} />
+        </div>
+    </div>
+  );
+}
+
+export default App;
