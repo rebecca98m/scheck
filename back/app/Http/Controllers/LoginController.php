19f5b9c740a2107173e604162f60ec5068403ca8
rebecca98m <schiavonrebecca@live.com>
2024-09-26 01:10:05 +0200

    fix

--- back/app/Http/Controllers/LoginController.php
+++ back/app/Http/Controllers/LoginController.php
@@ -18,6 +18,7 @@ class LoginController extends Controller
         ]);
 
         if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
             return Response::response(Auth::user());
         }
 
